5f265394dacecc09e93033127b78bb74a8e070d5
gigi <giovannaggs@gmail.com>
2022-07-18 12:32:48 +0200

    First-update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Popup from './Popup';
+
 
 function App() {
+  const popupStyle = {left:"10px"};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Pop-up Game</h1>
+        <button>Start</button>
       </header>
-    </div>
+      <main> 
+        <Popup className="popup" style={popupStyle}/>
+      </main>
+    </>
   );
 }
 
---
+++ src/Popup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Popup = () => {
+    return (<button style = {{left:"10px"}}>Click here!</button>)
+};
+
+export default Popup;
