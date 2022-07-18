454adf3f00d1f5cd81be5aeb26efa177febb474e
Nettester <github@gluch.eu>
2022-07-18 15:30:24 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import './index.css';
 import Popup from './Popup';
 
 
 function App() {
-  
+
+  const [reset, setReset] = useState(50);
+
+  const clickHandler = () => {
+    setReset(50)
+  }
+
   return (
     <>
       <header>
         <h1>Pop-up Game</h1>
-        <button>Start</button>
+        <button className='buttonStart' onClick={clickHandler}>Start</button>
       </header>
       <main> 
-        <Popup />
+        <Popup xLeft={reset} yTop={reset}/>
       </main>
     </>
   );
--- src/Popup.js
+++ src/Popup.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Popup = () => {
-    const popupStyle = {left:"10%", top: "80%"};
+const Popup = ({xLeft,yTop}) => {
+    
+    console.log({"left": xLeft + " " + yTop})
+    const [left, setLeft] = useState(xLeft);
+    const [top, setTop] = useState(yTop);
+ 
+    const popupStyle = {left: left+"%", top: top+"%"};
 
+    
+
+    const randomNum = (max, min) => {
+        return Math.floor(Math.random()*(max - min +1) + min)
+    }
+
+    const clickHandler = () => {
+        const min = 5;
+        const max = 80;
+        const randomLeft = randomNum(min,max);
+        const randomTop = randomNum(min,max);
+
+        setLeft(randomLeft);
+        setTop(randomTop);
+    }
     return (
         <div>
-            <button className="popup" style={popupStyle}>Click here!</button>
+            <button onClick={clickHandler} className="popup" style={popupStyle}>Click me!</button>
         </div>
     )
 };
